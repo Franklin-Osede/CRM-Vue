4bd5c415ca013b96b6e10d0bd596a6deeb6ae04f
Franklin <franklin.op@hotmail.com>
2024-04-29 17:36:43 +0200

    added submit functionality

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -8,12 +8,16 @@ defineProps ({
     type: String
   }
 })
+
+const handleSubmit = (data)=>{
+
+}
 </script>
 
 
 <template>
     <div>
-      <div class="flex justify-end">
+      <div class="flex justify-end ">
         <RouterLink to="inicio">
           Volver
           </RouterLink>
@@ -24,16 +28,61 @@ defineProps ({
         <div class="mx-auto md:w-2/3 py-20 px-6">
         <FormKit
         type="form"
+        submit-label="Agregar Cliente"
+        incomplete-message="Nose pudo enviar, revisa todos los campos"
+        @submit="handleSubmit"
         >
         <FormKit
             type="text"
             label="Nombre"
+            name="nombre"
             placeholder="Nombre de Cliente"
-            help= "Coloca el Nonbre del Cliente que deseas registrar"
             validation="required"
-            :validation-message="{required: 'El Nombre del Cliente es Obligatorio'}
-            "
+            :validation-messages="{required: 'El Nombre del Cliente es Obligatorio'}"
              />
+
+            <FormKit
+            type="text"
+            label="Apellido"
+            name="apellido"
+            placeholder="Apellido de Cliente"
+            validation="required"
+            :validation-messages="{required: 'El Apellido del Cliente es Obligatorio',email:'Coloca un email válido'}"
+             />   
+
+           <FormKit
+            type="text"
+            label="Email"
+            name="email"
+            placeholder="Email de Cliente"
+            validation="required|email"
+            :validation-messages="{required: 'El Email del Cliente es Obligatorio'}"
+             />   
+
+            <FormKit
+            type="text"
+            label="Teléfono"
+            name="telefono"
+            placeholder="Teléfono: XXX-XXX-XXXX"
+            validation="?matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+            :validation-messages="{matches: 'El Formato no es válido'}"
+             />    
+
+            <FormKit
+            type="text"
+            name="empresa"
+            label="Empresa"
+            placeholder="Empresa de Cliente"
+             />   
+
+
+            <FormKit
+            type="text"
+            name="puesto"
+            label="Puesto"
+            placeholder="Puesto de Cliente"
+             />  
+
 
         </FormKit>
         </div>
@@ -42,5 +91,9 @@ defineProps ({
     </div>
 </template>
 
-
+<style>
+  .formkit-wrapper {
+    max-width: 100%;
+  }
+</style>
  
